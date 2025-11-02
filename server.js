22af0ba989abf84bf2e0725ab655463dd98cb7ff
Rithvik Sharma <rsha264@aucklanduni.ac.nz>
2025-11-03 12:59:28 +1300

    Initial proj setup

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+// === EXAMPLE === 
+//      for when we start task 7
+
+// Import routes at top
+// const carValueRoute = require('./api1/carValueRoute');
+
+// Add Route
+// app.use('/api1', carValueRoute);
+// ===============
+
+app.listen(3000, () => console.log('Server running on port 3000'));
